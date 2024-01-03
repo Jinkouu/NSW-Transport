b6ca3097b038579ff6be83a173e6ed15653be2fd
Jinkouu <jonathan0174@live.com>
2024-01-03 13:25:30 +1100

    node js mainframe

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html'})
-    fs.readFile('index.html', function(error, data) {
-        if(error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found') //normally load a html file that says this
-        }
-        else{
-            res.write(data)
-        }
-        res.end()
-    })
-    //res.write('Hello Node')
+const APIKEY = process.env.NSW_APIKEY
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/weather', (req, res) => {
+
 })
 
-server.listen(port, function(error) {
-    if (error) {
-        console.log('Something went wrong', error)
-    }
-    else{
-        console.log('Server is listening on port ' + port)
-    }
+app.listen(3000, () => {
+    console.log('Server started')
 })
