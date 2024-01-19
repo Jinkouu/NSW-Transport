866d43d8dd3d7a6726332cf3329bdeffa188f853
Jinkouu <jonathan0174@live.com>
2024-01-19 22:06:57 +1100

    start search results dropdown

--- public/script.js
+++ public/script.js
@@ -18,56 +18,80 @@
 //    });
     
 
-const refreshBtn = document.getElementById("refresh-button")
-const searchBtn = document.getElementById("search-button")
-const startSearch = document.getElementById("start-search")
-const endSearch = document.getElementById("end-search")
-const startDropdownSearch = document.getElementById("start-search-dropdown")
+const refreshBtn = document.getElementById("refresh-button");
+const searchBtn = document.getElementById("search-button");
+const startSearch = document.getElementById("start-search");
+const endSearch = document.getElementById("end-search");
+const startDropdownSearch = document.getElementById("start-search-dropdown");
+const startSearchBox = document.getElementById("start-search-dropdown-search");
 
-startDropdownSearch.addEventListener("input", ()=>{
-
-})
-
+//start search dropdown
 startSearch.addEventListener("click", ()=>{
-    startDropdownSearch.style.display = (startDropdownSearch.style.display === "none") ? "flex" : "none";
-})
+    startSearch.blur();
+    startDropdownSearch.style.display = "flex";
+    startSearchBox.focus();
+    var val = startSearch.value;
+    startSearchBox.value = '';
+    startSearchBox.value = val;
+});
+startDropdownSearch.addEventListener("click", (e) =>{
+    e.stopPropagation();
+});
+document.addEventListener("mouseup", (e)=> {
+    if(e.target != startDropdownSearch && e.target.parentNode != startDropdownSearch){
+        startDropdownSearch.style.display = "none";
+    }
+});
 
-//async function check()
+//start search dropdown
+//api call to find valid locations
+startSearchBox.addEventListener("keyup", ()=>{
+    const location = startSearchBox.value;
+    var stations = [];
+    if(location.length){
+        const params = {
+            outputFormat: 'rapidJSON',
+            type_sf: 'any',
+            name_sf: `${location}`,
+            coordOutputFormat: 'EPSG:4326',
+            anyMaxSizeHitList: 5
+        };
+        const queryString = new URLSearchParams(params).toString();
+        fetch('/stopFinder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                queryString: `${queryString}`
+            })
+        }).then(res => res.json()).then(data => {
+            //console.log(data);
+            data.forEach((entry) => stations.push(entry.name));
+            console.log(stations);
+            displayStartResult(stations);
+        })
+    }
+})
+const resultsBox = document.querySelector(".result-box");
+//updates the html to add location to the dropdown
+function displayStartResult(result){
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+//selects the corresponding input
+function selectInput(list){
+    startSearch.value = list.innerHTML;
+    startDropdownSearch.style.display = "none";
+}
+
 
 searchBtn.addEventListener("click", ()=>{
-    const location = startSearch.value;
-    const params = {
-        outputFormat: 'rapidJSON',
-        type_sf: 'any',
-        name_sf: `${location}`,
-        coordOutputFormat: 'EPSG:4326',
-        anyMaxSizeHitList: 10
-    };
-    const queryString = new URLSearchParams(params).toString();
-    fetch('/stopFinder', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            queryString: `${queryString}`
-        })
-    }).then(res => res.json()).then(data => {
-        console.log(data);
-        var bestMatch = null; 
-        var locations = data;
-        //console.log(locations);
-        bestMatch = locations.find(location => location.isBest === true);
-
-        if (bestMatch){
-            const locationId = bestMatch.id;
-            const name = bestMatch.name;
-            console.log(locationId);
-            console.log(name);
-        }
-        setLocationData(data, place.formatted_address)
-    })
+    
 })
 
 // Call the main function
