b978b0dc15d00200cfa8295eb8cea43824b21f54
Jinkouu <jonathan0174@live.com>
2024-01-19 22:55:43 +1100

    updated for corresponding end results

--- public/script.js
+++ public/script.js
@@ -1,54 +1,9 @@
-//fetch(url, {
-//    method: 'GET',
-//    headers: {
-//        'Authorization': 'Bearer ${apiKey}'
-//    },
-//})
-//    .then(response => {
-//        if (!response.ok) {
-//            throw new Error(`Request failed with status: ${response.status}`)
-//        }
-//        return response.json();
-//    })
-//    .then(data => {
-//        console.log(data);
-//    })
-//    .catch(error => {
-//        console.error(error);
-//    });
-    
-
 const refreshBtn = document.getElementById("refresh-button");
 const searchBtn = document.getElementById("search-button");
-const startSearch = document.getElementById("start-search");
-const endSearch = document.getElementById("end-search");
-const startDropdownSearch = document.getElementById("start-search-dropdown");
-const startSearchBox = document.getElementById("start-search-dropdown-search");
 
-//start search dropdown
-startSearch.addEventListener("click", ()=>{
-    startSearch.blur();
-    startDropdownSearch.style.display = "flex";
-    startSearchBox.focus();
-    var val = startSearch.value;
-    startSearchBox.value = '';
-    startSearchBox.value = val;
-});
-startDropdownSearch.addEventListener("click", (e) =>{
-    e.stopPropagation();
-});
-document.addEventListener("mouseup", (e)=> {
-    if(e.target != startDropdownSearch && e.target.parentNode != startDropdownSearch){
-        startDropdownSearch.style.display = "none";
-    }
-});
-
-//start search dropdown
-//api call to find valid locations
-startSearchBox.addEventListener("keyup", ()=>{
-    const location = startSearchBox.value;
-    var stations = [];
-    if(location.length){
+//api call to find valid locations - calls server.js /stopFinder
+function fetchData(location) {
+    return new Promise((resolve, reject) => {
         const params = {
             outputFormat: 'rapidJSON',
             type_sf: 'any',
@@ -66,38 +21,154 @@ startSearchBox.addEventListener("keyup", ()=>{
             body: JSON.stringify({
                 queryString: `${queryString}`
             })
-        }).then(res => res.json()).then(data => {
-            //console.log(data);
-            data.forEach((entry) => stations.push(entry.name));
-            console.log(stations);
-            displayStartResult(stations);
         })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+}
+//api call to find valid locations
+
+//----------start search related--------------
+const startSearch = document.getElementById("start-search");
+const startDropdownSearch = document.getElementById("start-search-dropdown");
+const startSearchBox = document.getElementById("start-search-dropdown-search");
+function completeStartSearch(){ //matches new input box values
+    startSearch.blur();
+    startDropdownSearch.style.display = "flex";
+    startSearchBox.focus();
+    var val = startSearch.value;
+    startSearchBox.value = '';
+    startSearchBox.value = val;
+}
+startSearch.addEventListener("click", ()=>{ //start search dropdown
+    completeStartSearch()
+    if (location.length) {
+        fetchData(location)
+            .then(data => {
+                data.forEach((entry) => stations.push(entry.name));
+                console.log(stations);
+                displayStartResult(stations);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+});
+startSearchBox.addEventListener('keypress', (e) =>{ 
+    if (e.key === 'Enter'){
+        startDropdownSearch.style.display = "none";
     }
 })
-const resultsBox = document.querySelector(".result-box");
+startSearchBox.addEventListener("keyup", ()=>{
+    const location = startSearchBox.value;
+    var stations = [];
+
+    if (location.length) {
+        fetchData(location)
+            .then(data => {
+                data.forEach((entry) => stations.push(entry.name));
+                console.log(stations);
+                displayStartResult(stations);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+})
+const startResultsBox = document.getElementById("start-result-box");
 //updates the html to add location to the dropdown
 function displayStartResult(result){
     const content = result.map((list) => {
-        return "<li onclick=selectInput(this)>" + list + "</li>";
+        return "<li onclick=selectStartInput(this)>" + list + "</li>";
     });
 
-    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+    startResultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
 }
 //selects the corresponding input
-function selectInput(list){
+function selectStartInput(list){
     startSearch.value = list.innerHTML;
     startDropdownSearch.style.display = "none";
 }
+//----------start search related--------------
 
+//-------------------endsearch related-------------------
+const endSearch = document.getElementById("end-search");
+const endDropdownSearch = document.getElementById("end-search-dropdown");
+const endSearchBox = document.getElementById("end-search-dropdown-search");
+function completeEndSearch(){
+    endSearch.blur();
+    endDropdownSearch.style.display = "flex";
+    endSearchBox.focus();
+    var val = endSearch.value;
+    endSearchBox.value = '';
+    endSearchBox.value = val;
+}
+endSearch.addEventListener("click", ()=>{
+    completeEndSearch()
+    if (location.length) {
+        fetchData(location)
+            .then(data => {
+                data.forEach((entry) => stations.push(entry.name));
+                console.log(stations);
+                displayEndResult(stations);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+});
+endSearchBox.addEventListener("keypress", (e) =>{
+    if (e.key === "Enter"){
+        endDropdownSearch.style.display = "none";
+    }
+})
+endSearchBox.addEventListener("keyup", ()=>{
+    const location = endSearchBox.value;
+    var stations = [];
+
+    if (location.length) {
+        fetchData(location)
+            .then(data => {
+                data.forEach((entry) => stations.push(entry.name));
+                console.log(stations);
+                displayEndResult(stations);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+})
+const endResultsBox = document.getElementById("end-result-box");
+//updates the html to add location to the dropdown
+function displayEndResult(result){
+    const content = result.map((list) => {
+        return "<li onclick=selectEndInput(this)>" + list + "</li>";
+    });
+
+    endResultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+//selects the corresponding input
+function selectEndInput(list){
+    endSearch.value = list.innerHTML;
+    endDropdownSearch.style.display = "none";
+}
+//-------------------endsearch related-------------------
+
+document.addEventListener("mouseup", (e)=> {
+    if(e.target != startDropdownSearch && e.target.parentNode != startDropdownSearch){
+        startDropdownSearch.style.display = "none";
+    }
+    if(e.target != endDropdownSearch && e.target.parentNode != endDropdownSearch){
+        endDropdownSearch.style.display = "none";
+    }
+});
 
 searchBtn.addEventListener("click", ()=>{
     
 })
 
-// Call the main function
 
-
-//refreshBtn.addEventListener("click", loadApi());
 
 
 
